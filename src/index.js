88a686283d03ca7b994a614e6fc9113dd1efcc4f
madhukodali23 <madhukanth838@gmail.com>
2025-05-29 03:55:46 +0000

    modified routing errors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/NxtTrendz">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
